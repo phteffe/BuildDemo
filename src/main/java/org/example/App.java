053aafb96cb93088c358246b67e154513fa6807a
Stefan Karlsson <phtefan.karlsson@gmail.com>
2023-01-21 17:03:05 +0100

    Add tests and coherent code to be in phase with Martin

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,7 +1,51 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello There!");
+
+    private int k = 0;
+
+    public int getK() {
+        return k;
     }
+
+    public void setK(int k) {
+        if (k < 0)
+            throw new IllegalArgumentException();
+        this.k = k;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        App app = (App) o;
+
+        return k == app.k;
+    }
+
+    @Override
+    public int hashCode() {
+        return k;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Write two numbers:");
+        Scanner scanner = new Scanner(System.in);
+        var a = scanner.nextInt();
+        var b = scanner.nextInt();
+
+        System.out.println(add(a,b));
+
+    }
+    public static int add (int a, int b){
+        return a + b;
+    }
+
+    public static boolean odd(int a){
+        return a % 2 == 1;
+    }
+
 }
--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,12 +1,43 @@
 package org.example;
 
+import nl.jqno.equalsverifier.EqualsVerifier;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
     @Test
     void test() {
         assertThat(true).isTrue();
     }
+
+    @Test
+    void addTwoNumReturnsSum() {
+        var result = App.add(1,1);
+
+        assertEquals(2,result);
+    }
+
+    @Test
+    void givenOddNumOddShouldReturnTrue() {
+        assertTrue(App.odd(3));
+    }
+    @Test
+    void givenEvenNumOddShouldReturnFalse() {
+        assertFalse(App.odd(2));
+    }
+
+    @Test
+    void setNegativeNumShouldThrowIllegalArgumentException() {
+        App app = new App();
+
+        assertThrows(IllegalArgumentException.class, () -> app.setK(-1));
+    }
+
+    @Test
+    void equalsContractForApp() {
+        EqualsVerifier.simple().forClass(App.class).verify();
+    }
+
 }
